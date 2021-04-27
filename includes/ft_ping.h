fa89a816bc13e69892e2b4cc0c093f79e7eedbd6
Angie Susan <asusan@mi-e3.msk.21-school.ru>
2021-04-27 22:47:02 +0300

    remove default param from GetCSum

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,13 +6,16 @@
 # include <stdint.h>
 # include <time.h>
 
+# include <stdlib.h>
 # include <errno.h>
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <sys/time.h>
 # include <netinet/ip_icmp.h>
+# include <netinet/ip.h>
 # include <netinet/in.h>
+
 # include <arpa/inet.h>
 # include <sys/select.h>
 # include <netdb.h>
