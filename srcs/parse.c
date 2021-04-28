ce88a7a12a34fd9d4df1a579c5b8bab8dced2bf4
Angie Susan <asusan@mi-e3.msk.21-school.ru>
2021-04-28 14:40:22 +0300

    asd

--- srcs/parse.c
+++ srcs/parse.c
@@ -14,8 +14,7 @@ BOOL	HostToIP(t_IcmpTargetType *targets)
 	ft_memset(targets->dst_addr, 0, INET_ADDRSTRLEN);
 	if (getaddrinfo(targets->addr_from_arg, NULL, &hints, &res) != 0)
 	{
-		if (res)
-			freeaddrinfo(res);
+		freeaddrinfo(res);
 		return (printExitWStr(targets->addr_from_arg,
 				": Name or service not known"));
 	}
